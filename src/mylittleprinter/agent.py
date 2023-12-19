137025b599f119e0326b2e8f113c9214138e5174
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-19 15:06:07 -0500

    added task scheduler in agent

--- src/mylittleprinter/agent.py
+++ src/mylittleprinter/agent.py
@@ -1,21 +1,37 @@
 
+import sched
+import time
 from mylittleprinter.activity import Activity
 from mylittleprinter.portfolio import AbstractPortfolio, SimulatedPortfolio
+
+RUN_TASK_DELAY = 5
+RUN_TASK_PRIORITY = 1
 
 class Agent(object):
     def __init__(self, cash: int) -> None:
         self.portfolio: AbstractPortfolio = SimulatedPortfolio(cash)
         self.activity: Activity = Activity()
+        self.scheduler = sched.scheduler(time.monotonic, time.sleep)
+
+        self._init_task_scheduler()
 
     def run_task(self) -> None:
         """
         Run periodic task.
         """
-        return NotImplementedError
+        print("Running periodic task...")
+        # add next task call to the queue
+        self.scheduler.enter(RUN_TASK_DELAY, RUN_TASK_PRIORITY, self.run_task)
+
+    def _init_task_scheduler(self):
+        self.scheduler.enter(RUN_TASK_DELAY, RUN_TASK_PRIORITY, self.run_task)
 
     def run(self) -> None:
         """
         Run periodic task scheduler.
         """
-        return NotImplementedError
+        print("Starting task scheduler...")
+        self.scheduler.run()
+        
+        
 
